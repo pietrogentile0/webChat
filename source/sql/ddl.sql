0007d6e7736f06ecbb8e4ac852fe4ba2f3ce5830
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-09 22:30:32 +0100

    created database and table

---
+++ source/sql/ddl.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Utenti(
+	id Integer AUTO_INCREMENT,
+    nome varchar(25),
+    cognome varchar(25),
+    username varchar(20),
+    email varchar(40),
+    passwd char(32),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE Chat(
+    id Integer AUTO_INCREMENT,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE Partecipanti(
+	idUtente Integer not null,
+    idChat Integer not null,
+    PRIMARY KEY (idUtente, idChat),
+    FOREIGN KEY (idUtente) REFERENCES utenti(id),
+    FOREIGN KEY (idChat) REFERENCES Chat(id)
+)
+
+CREATE TABLE Messaggi(
+	id integer AUTO_INCREMENT,
+    idMittente integer not null,
+    idChat integer not null,
+    testo varchar(500) not null,
+  	PRIMARY key (id),
+    FOREIGN KEY (idMittente) REFERENCES utenti(id),
+    FOREIGN key (idChat) REFERENCES chat(id)
+)
