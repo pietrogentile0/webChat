d5768106599559fc94182427d246249c716beb8f
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-10 17:57:19 +0100

    terminata stesura codice per emittente JWT

--- source/login/DatabaseService.php
+++ source/login/DatabaseService.php
@@ -8,15 +8,15 @@ class DatabaseService{
     private $connection;
 
     function __construct($host, $user, $password, $database){
-        $this->host;
-        $this->user;
-        $this->password;
-        $this->database;
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->database = $database;
     }
 
     public function getConnection(){
         try{
-            $this->connection = new mysqli($host, $user, $password, $database);
+            $this->connection = new mysqli($this->host, $this->user, $this->password, $this->database);
         } catch (Exception $e){
             throw new Exception($e);
         }
--- source/login/login.php
+++ source/login/login.php
@@ -3,18 +3,62 @@ require "DatabaseService.php";
 require "../../jws/vendor/autoload.php";
 use \Firebase\JWT\JWT;
 
-$data = json_decode(file_get_contents("php://input"));
+$params = json_decode(file_get_contents("php://input"), true);
 
-if((isset($data["email"]) || isset($data["username"])) && isset($data["password"])){
-    $username = isset($data["email"]) ? $data["email"] : $data["username"];
-    $password = $data["password"];
+if((isset($params["email"]) || isset($params["username"])) && isset($params["password"])){
+    $username = isset($params["email"]) ? $params["email"] : $params["username"];
+    $password = $params["password"];
 
     try{
         $dbService = new DatabaseService("localhost", "root", "", "webchat");
         $db = $dbService->getConnection();
+        
+        $query = "SELECT id, username, password FROM Utenti WHERE username = '$username' LIMIT 1";
+        $rs = $db->query($query);
+        
+        if($rs->num_rows > 0){
+            $data = $res->fetch_assoc();
+
+            $id = $data["id"];
+            $dbUsername = $data["username"];
+            $dbPassword = $data["password"];
+
+            if(password_verify($password, $dbPassword)){
+                $privateKey = file_get_contents("./rsa_keys/private_key.txt");
+                $issuer_claim = "chaliwhat";
+                $issuedAt_claim = time();
+                $notBefore_claim = $issuedAt_claim;
+                $expire_claim = $issuedAt_claim + 60;
+
+                $token = array(
+                    "iss" => $issuer_claim,
+                    "iat" => $issuedAt_claim,
+                    "nbf" => $notBefore_claim,
+                    "exp" => $expire_claim,
+                    "data" => array(
+                        "id" => $id,
+                        "username"=> $dbUsername
+                    )
+                );
+
+                http_response_code(200);
+                $jwt = JWT::encode($token, $privateKey);
+                echo json_encode(
+                    array(
+                        "message" => "Successful login",
+                        "jwt" => $jwt,  
+                        "username" => $username
+                    )
+                );
+            }
+        } else {
+            http_response_code(401);
+            echo json_encode(array("error"=>"Your credentials aren't on our database"));
+        }
+
+        $db->close();
     }catch(Exception $e){
-        echo(json_encode(array("error"=>$e)));
+        echo(json_encode(array("error"=>"$e")));
     }
 
-    $query = "SELECT id, username, password FROM Utenti WHERE username = '$username' LIMIT 1";
 }
