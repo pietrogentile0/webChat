3ef95093c2ba9840b241dfbdc100973be7154f4f
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-20 10:58:36 +0100

    inizio implementazione invio conversazioni di un utente, da migliorare per prendere id dal token

--- source/server/upload-conversations/upload-conversations.php
+++ source/server/upload-conversations/upload-conversations.php
@@ -6,13 +6,33 @@
         redirectToLogin();
     }
 
-    $params = JSON.decode(file_get_contents("php://input"));
+    $params = json_decode(file_get_contents("php://input"), true);
     if(isset($params["userId"])){
         $userId = $params["userId"];
         
         $dbService = new DatabaseService("localhost", "root", "", "chaliwhat");
-        $db->getConnection();
+        $db = $dbService->getConnection();
 
-        $query = "";
+        $getConversations = "
+            SELECT c.id, COALESCE(c.nome, u.nome) AS nome
+            FROM utenti AS u,
+                partecipanti AS p,
+                (SELECT c.* FROM partecipanti AS p, chat as c WHERE p.idChat = c.id AND p.idUtente = 1) AS c	-- trova tutte le chat di un utente
+            WHERE u.id = p.idUtente
+                AND p.idChat = c.id
+                AND u.id <> 1
+            GROUP BY c.id
+        ";
+        
+        if(($conversations_rs = $db->query($getConversations))){
+            $conversations = [];
+
+            while(($temp = $conversations_rs->fetch_assoc())){
+                array_push($conversations, $temp);
+            }
+            
+            echo json_encode($conversations);
+        }
+
+        $dbService->closeConnection();
     }
-
