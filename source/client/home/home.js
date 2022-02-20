31ab7ec4668a7fe4d0fe3ed0e5306d2cdd479153
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-20 11:02:32 +0100

    spostamento funzioni specifiche in file specifico

--- source/client/home/home.js
+++ source/client/home/home.js
@@ -1,27 +1,11 @@
+const userData = getJwtPayload(getJwt(document.cookie)).data;
+const currentChat = null;
+
 // reads the user's name from JWT and shows it with the user icon
-function setIconName() {
-    const payload = getJwtPayload(getJwt(document.cookie));
-    const userData = payload.data;
-
+function setIconName(name) {
     const usernameLocation = document.querySelector("#username");
-    usernameLocation.textContent = capitalLetter(userData["name"]);
+
+    usernameLocation.textContent = capitalLetter(name);
 }
 
-setIconName();
-
-async function downloadConversations() {
-    const userId = getJwtPayload().id;
-    const res = await fetch("http://locahost/chaliwhat/source/server/uploadConversations.php", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "userId": userId
-        })
-    });
-}
-
-function downloadChat() {
-
-}
+setIconName(userData.name);
