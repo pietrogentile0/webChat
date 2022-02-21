921383dc0a833632e905fee7856bea85ab5c4e09
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-21 16:20:39 +0100

    migliorato codice e aggiunta invio username al client

--- source/server/upload-conversations/upload-conversations.php
+++ source/server/upload-conversations/upload-conversations.php
@@ -1,38 +1,36 @@
 <?php
-    require "./../jwt-controller/jwt-controller.php";
-    require("./../login/login-token/DatabaseService.php");
+    require $_SERVER['DOCUMENT_ROOT']."chaliwhat/source/server/jwt-manager/jwt-controller.php";
+    require $_SERVER['DOCUMENT_ROOT']."chaliwhat/source/server/login/login-token/DatabaseService.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."chaliwhat/source/server/jwt-manager/jwt-getInfo.php"; // altimenti lo prende 2 volte
 
     if(!isLogged()){
         redirectToLogin();
     }
 
-    $params = json_decode(file_get_contents("php://input"), true);
-    if(isset($params["userId"])){
-        $userId = $params["userId"];
-        
-        $dbService = new DatabaseService("localhost", "root", "", "chaliwhat");
-        $db = $dbService->getConnection();
+    $userId = getUserIdFromJwt(getJwt());
+    
+    $dbService = new DatabaseService("localhost", "root", "", "chaliwhat");
+    $db = $dbService->getConnection();
 
-        $getConversations = "
-            SELECT c.id, COALESCE(c.nome, u.nome) AS nome
-            FROM utenti AS u,
-                partecipanti AS p,
-                (SELECT c.* FROM partecipanti AS p, chat as c WHERE p.idChat = c.id AND p.idUtente = 1) AS c	-- trova tutte le chat di un utente
-            WHERE u.id = p.idUtente
-                AND p.idChat = c.id
-                AND u.id <> 1
-            GROUP BY c.id
-        ";
-        
-        if(($conversations_rs = $db->query($getConversations))){
-            $conversations = [];
+    $getConversations = "
+        SELECT c.id, COALESCE(c.nome, u.nome) AS nome, IF(ISNULL(c.nome), u.username, c.nome) AS username
+        FROM utenti AS u,
+            partecipanti AS p,
+            (SELECT c.* FROM partecipanti AS p, chat as c WHERE p.idChat = c.id AND p.idUtente = $userId) AS c
+        WHERE u.id = p.idUtente
+            AND p.idChat = c.id
+            AND u.id <> $userId
+        GROUP BY c.id
+    ";
+    
+    if(($conversations_rs = $db->query($getConversations))){
+        $conversations = [];
 
-            while(($temp = $conversations_rs->fetch_assoc())){
-                array_push($conversations, $temp);
-            }
-            
-            echo json_encode($conversations);
+        while(($temp = $conversations_rs->fetch_assoc())){
+            array_push($conversations, $temp);
         }
-
-        $dbService->closeConnection();
+        
+        echo json_encode($conversations);
     }
+
+    $dbService->closeConnection();
