7bd3bda022d0c80a3b63056871d8bb2650a20579
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-21 16:32:15 +0100

    aggiunta redirect alla home se cliccata l'icona e implementato logout utente

--- source/client/home/home.js
+++ source/client/home/home.js
@@ -9,3 +9,14 @@ function setIconName(name) {
 }
 
 setIconName(userData.name);
+
+document.querySelector("#logo").addEventListener("click", () => {
+    location.href = "http://localhost/chaliwhat/source/client/home/home.php";
+});
+
+document.querySelector("#logout").addEventListener("click", () => {
+    console.log(document.cookie);
+    document.cookie = "x-chaliwhat-token=; expires=Thu, 01 Jan 1970 00:00:00 CET; path=/chaliwhat; domain=localhost";
+    location.href = "http://localhost/chaliwhat/source/client/login/login_page.php";
+});
+
