8d8daa41927fe4aff0842a9901d1ad4bb921fc2a
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-23 19:17:48 +0100

    implementato sistema gestione chat

--- source/client/home/chat.js
+++ source/client/home/chat.js
@@ -1,20 +1,62 @@
-function createMessageBox(message, senderName) {
-    const div = document.createElement("div");
-    div.classList.add("message");
-    const nameDiv = document.createElement("div");
-    // nameDiv.classList;
-    const messageDiv = document.createElement("div");
+/**Updates current chat (the one to show in the chat tab)
+ * 
+ * @param {number} chatId 
+ */
+function setCurrentChatId(chatId) {
+    currentChatId = chatId;
 }
 
-async function downloadChatWith(chatId) {
+/**Builds the container for a message in the chat
+ * 
+ * @param {number} messageId to show
+ * @param {string} text of the message
+ * @param {number} [senderId] ID of the sender
+ * @param {string} [sender] name of the sender
+ * 
+ * @return {Element} message container
+ */
+function createMessageContainer(messageId, text, senderId = null, sender = null) {
+    // create message container
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.id = messageId;
+
+    // if I sent this message, it must be shown on the right
+    if (senderId == getMyId()) {
+        message.classList.add("message-from-me");
+    }
+
+    // if is a group's message, add sender's username
+    if (sender != null) {
+        const usernameContainer = document.createElement("div");
+        usernameContainer.classList.add = "sender";
+        usernameContainer.textContent = sender;
+        message.append(usernameContainer);
+    }
+
+    // add message's text
+    const textContainer = document.createElement("div");
+    textContainer.classList.add = "textContent";
+    textContainer.textContent = text;
+    message.append(textContainer);
+
+    return message;
+}
+
+/** Downloads chat's messages from server
+ * 
+ * @param {number} chatId ID of the chat you want
+ * @returns 
+ */
+async function downloadChatWith(conversationId) {
     const res = await fetch("http://localhost/chaliwhat/source/server/upload-chat/upload-chat.php", {
         method: "post",
         headers: {
             "Content-Type": "application/json"
         },
-        body: {
-            "chatId": chatId
-        }
+        body: JSON.stringify({
+            "chatId": conversationId
+        })
     });
 
     if (res.status == 200) {
@@ -25,36 +67,76 @@ async function downloadChatWith(chatId) {
     }
 }
 
-async function getChatWith(chatId) {
-    let currentChat = chatId;
-    alert("searching for chat number " + chatId);
+/**Sets te title of Chat tab
+ * 
+ * @param {*} name Name of the conversation
+ * @param {*} username Username of the conversation
+ */
+function setChatTitle(name, username) {
+    const chatName = document.querySelector("#chat-name");
+    chatName.textContent = capitalLetter(name);
+    const chatUsername = document.querySelector("#chat-username");
+    chatUsername.textContent = username;
 }
 
-async function sendMessage(message, idSender, idChat) {
+/** When a conversation is selected, shows it in the right tab (chat tab)
+ * @param {Number} conversationId ID of the selected conversation
+ * @param {string} name Name of the conversation
+ * @param {string} username Username of the person in the conversation (default is null if group)
+ */
+async function getChatWith(conversationId, name, username = null) {
+    const chatContainer = document.querySelector("#chat-container");
+
+    clearElement(chatContainer);
+    setChatTitle(name, username);
+
+    setCurrentChatId(conversationId);
+    const messages = await downloadChatWith(currentChatId);
+
+    for (let i of messages) {
+        const message = createMessageContainer(i.id, i.testo, i.idMittente, i.username_mittente);
+        chatContainer.prepend(message); //aggiunge all'inizio
+    }
+}
+
+/** Sends a message to the database
+ * 
+ * @param {string} message 
+ * @param {number} idChat 
+ */
+async function sendMessageToServer(message, idChat) {
     const res = await fetch("http://localhost/chaliwhat/source/server/new-message/new-message.php", {
         method: "post",
         headers: {
             "Content-Type": "application/json"
         },
-        body: {
+        body: JSON.stringify({
             "message": message,
-            "idSender": idSender,
             "idChat": idChat
-        }
+        })
     });
 
+    const info = await res.json();
     if (res.status == 200) {
-        createMessageBox(message, userData.name);
+        return info.id;
     } else {
-        const info = await res.json();
-        alert(info.error);
+        throw new Error(info.error);
     }
 }
 
-document.querySelector("#send-message").addEventListener("click", () => {
-    const message = document.querySelector("#message").value;
-    const idSender = userData["id"];
+document.querySelector("#send-message").addEventListener("click", async () => {
+    if (currentChatId != null) {
+        const chatContainer = document.querySelector("#chat-container");
+        const message = document.querySelector("#message").value;
+        document.querySelector("#message").value = "";
 
-    sendMessage(message, idSender, currentChat);
+        if (message.length > 0) {
+            try {
+                const messageId = await sendMessageToServer(message, currentChatId);
+                const messageContainer = createMessageContainer(messageId, message, userData.id);
+                chatContainer.prepend(messageContainer);
+            } catch (error) { alert(error); }
+        }
+    }
 });
 
