3ed7d49076c8d89cd4764d62ae24cca223f731e3
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-23 19:14:48 +0100

    aggiunta query per ottenere messaggi

--- source/server/sql/ql.sql
+++ source/server/sql/ql.sql
@@ -1,8 +1,3 @@
--- get all conversations of user (non con il nome dell'utente se non è un gruppo)
-SELECT c.*
-FROM partecipanti AS p, chat as c
-WHERE p.idChat = c.id AND p.idUtente = 1
-
 -- per ottenere tutte le chat di un utente (sia gruppo sia utente)
 -- se una chat ha un nome è un gruppo e viene restituito il suo nome
 -- altrimenti è una chat semplice (2 utenti) e viene restituito il nome dell'altro utente e il suo username (null nel caso sia un gruppo)
@@ -14,3 +9,10 @@ WHERE u.id = p.idUtente
     AND p.idChat = c.id
     AND u.id <> $userId
 GROUP BY c.id
+
+-- ottenere tutti i messaggi di una conversazione (con il nome del mittente nel caso fosse un gruppo)
+SELECT m.id, m.testo, IF(ISNULL(c.nome), null, username) AS username_mittente
+FROM messaggi AS m, utenti AS u, chat AS c
+WHERE u.id = m.idMittente
+    AND m.idChat = c.id
+    AND idChat = $chatId
