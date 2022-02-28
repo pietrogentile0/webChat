cc94281bed9d047830521063d140f83587f1862a
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-28 11:22:55 +0100

    implementata funzionalità di live chat parte client

---
+++ source/client/home/live-chat.js
@@ -0,0 +1,34 @@
+const ws = new WebSocket("ws://localhost:3001");
+
+ws.onopen = () => {
+    letKnowId(getMyId());
+}
+
+ws.addEventListener("message", (message) => {
+    const chatContainer = document.querySelector("#chat-container");
+
+    const body = JSON.parse(message.data);
+    if (body.function == "new-message") {
+        const messageId = body.messageId;
+        const text = body.text;
+        const senderId = body.senderId;
+        const senderUsername = body.senderUsername;
+
+        const messageContainer = createMessageContainer(messageId, text, senderId, senderUsername);
+        chatContainer.prepend(messageContainer);
+    }
+});
+
+function letKnowId(userId) {
+    ws.send(JSON.stringify({
+        function: "update-id",
+        userId: userId
+    }));
+}
+
+function letKnowCurrentChat(chatId) {
+    ws.send(JSON.stringify({
+        function: "update-current-chat",
+        currentChat: currentChatId
+    }));
+}
