2177d79224d313ab9a6fa1dd55c95f91392e0073
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-28 11:23:57 +0100

    aggiunta parte di invio nuovo messaggio a server node che gestisce la parte della chat live

--- source/server/new-message/new-message.php
+++ source/server/new-message/new-message.php
@@ -7,6 +7,43 @@
         redirectToLogin();
     }
 
+    function makeHttpnotification($url, $headers, $body){
+        $url = $url."/new-message";
+
+        $ns = curl_init($url);    // notification service
+
+        curl_setopt($ns, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ns, CURLOPT_POSTFIELDS, json_encode($body));
+        
+        $response = curl_exec($ns);
+        curl_close($ns);
+
+        return $response;
+    }
+
+    function notifyToClient($serverUrl, $chatId, $messageId, $text, $senderId=null, $senderUsername=null){
+        $headers = array(
+            "Content-Type: application/json",
+        );
+
+        $body = array(
+            "chatId"=>$chatId,
+            "messageId"=>$messageId,
+            "text"=>$text,
+            "senderId"=>$senderId,
+            "senderUsername"=>$senderUsername
+        );
+
+        $response =  makeHttpNotification($serverUrl, $headers, $body);
+
+        if($response){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     $params = json_decode(file_get_contents("php://input"), true);
     
     if(isset($params["message"]) && isset($params["idChat"])){
@@ -25,12 +62,14 @@
             $newMessageId = $db->query($getMessageId)->fetch_assoc()["id"];
 
             http_response_code(200);
-            echo json_encode(array("chatId"=>$newMessageId));
+            echo json_encode(array("messageId"=>$newMessageId));
+
+            notifyToClient("http://localhost:3000", $idChat, $newMessageId, $message, $idSender);
         }catch(Exception $e){
             http_response_code(500);
             echo json_encode(array("error"=>$e->getMessage()));
         }
 
         $dbService->closeConnection();
-    } 
+    }
     else { http_response_code(400); }
