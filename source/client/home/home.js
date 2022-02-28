377964b449fb3c31cd0c5179e53a148d5db0697f
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-28 11:20:59 +0100

    aggiunta comunicazione di disconnessione dal client al server node

--- source/client/home/home.js
+++ source/client/home/home.js
@@ -14,7 +14,12 @@ document.querySelector("#logo").addEventListener("click", () => {
 });
 
 document.querySelector("#logout").addEventListener("click", () => {
-    console.log(document.cookie);
+    // before logging out, I need to let know Node that I'm going to close WebSocket
+    // so it can delete my WebSocket from his list of active ones
+    ws.send(JSON.stringify({
+        function: "remove-this-socket"
+    }))
+
     document.cookie = "x-chaliwhat-token=; expires=Thu, 01 Jan 1970 00:00:00 CET; path=/chaliwhat; domain=localhost";
     location.href = "http://localhost/chaliwhat/source/client/login/login_page.php";
 });
