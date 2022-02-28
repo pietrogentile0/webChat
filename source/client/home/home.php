8825af1afb0f0824dadce25939630136e7d25539
pietrogentile0 <pietrogentile03@gmail.com>
2022-02-28 11:21:40 +0100

    spostata inclusione file js per evitare problemi di dipendenza

--- source/client/home/home.php
+++ source/client/home/home.php
@@ -57,10 +57,10 @@
     </div>
 
     <script src="http://localhost/chaliwhat/source/client/utilities.js"></script>
+    <script src="http://localhost/chaliwhat/source/client/home/live-chat.js"></script>
     <script src="http://localhost/chaliwhat/source/client/home/home.js"></script>
     <script src="http://localhost/chaliwhat/source/client/home/chat.js"></script>
     <script src="http://localhost/chaliwhat/source/client/home/conversation.js"></script>
-    <script src="http://localhost/chaliwhat/source/client/home/live-chat.js"></script>
     <!-- <script src="http://localhost/chaliwhat/source/client/home/new-group.js"></script> -->
 </body>
 
